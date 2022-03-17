96f34757a7eee3a8b8921b378c34e9504b0cd75a
Dauleshwar Kumar <dauleshwar.kumar@freecharge.com>
2022-03-17 16:12:08 +0530

    electron-pos-printer

--- src/components/Patients/PatientData.js
+++ src/components/Patients/PatientData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -58,6 +58,61 @@ const SubTitle = ({ title, value }) => {
 }
 export default function PatientData({ patient, setPrint }) {
   const classes = useStyles();
+  // const [finalData, setfinalData] = useState([{}])
+  console.log(patient)
+  const doPrint = () => {
+    const finalData = [{
+      type: 'text',
+      value: 'ANGELINA LIFE CARE HOSPITAL',
+      style: `text-align:center;`,
+      css: { "font-weight": "700", "font-size": "18px" }
+    }, {
+      type: 'text',
+      value: 'Mofassil Mod, Khizarsarai Road',
+      style: `text-align:center;`,
+      css: { "font-weight": "500", "font-size": "14px" }
+    }, {
+      type: 'text',
+      value: 'In front of vishal Petrol Pump',
+      style: `text-align:center;`,
+      css: { "font-weight": "500", "font-size": "14px" }
+    }, {
+      type: 'text',
+      value: 'Mob no - 9472643340',
+      style: `text-align:center;`,
+      css: { "font-weight": "500", "font-size": "16px" }
+    }, {
+      type: 'text',
+      value: 'OPD Slip',
+      style: `text-align:center;`,
+      css: { "font-weight": "600", "font-size": "16px" }
+    }, , {
+      type: 'table',
+      style: 'border: 1px solid #ddd',
+      tableBody: [
+        [{ type: 'text', value: "Doctor Name" }, { type: 'text', value: "Dr. Angelina" }],
+        [{ type: 'text', value: "Patient Name" }, { type: 'text', value: patient.Name }],
+        [{ type: 'text', value: `Uid :  ${patient.UId}` }, { type: 'text', valu: ` Uid : patient.Tid` }],
+        [{ type: 'text', value: "Address" }, { type: 'text', value: patient.Address }],
+        [{ type: 'text', value: "Age" }, { type: 'text', value: patient.Age }],
+        [{ type: 'text', value: "Gender" }, { type: 'text', value: patient.sex }],
+        [{ type: 'text', value: "Temp." }, { type: 'text', value: patient.CurrentTemp }],
+        [{ type: 'text', value: "BP" }, { type: 'text', value: patient.CurrentBp }],
+        [{ type: 'text', value: "Oxygen" }, { type: 'text', value: patient.CurrentOxygen }],
+        [{
+          type: 'text', value: `dateOfAppoint :  ${patient.dateOfAppoint}`
+        }, {
+          type: 'text', value: `dateOfBooking :  ${patient.dateOfBooking}`
+        }],
+        [{ type: 'text', value: "Fee" }, { type: 'text', value: "200 Rs" }],
+      ],
+      tableHeaderStyle: 'background-color: #000; color: white;',
+      tableBodyStyle: 'border: 0.5px solid #ddd',
+      tableFooterStyle: 'background-color: #000; color: white;',
+    }]
+    setPrint(finalData)
+  }
+
   console.log(patient, "patient in data")
   return (
     <Card className={classes.root} variant="outlined">
@@ -77,7 +132,7 @@ export default function PatientData({ patient, setPrint }) {
         </Box>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={() => setPrint(patient)}>Print</Button>
+        <Button size="small" onClick={doPrint}>Print</Button>
       </CardActions>
     </Card>
   );
